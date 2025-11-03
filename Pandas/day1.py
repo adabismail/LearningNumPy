8a579ae7b8835a74b8a92db1df78cfb2e1324b6a
adabismail <adabismail000@gmail.com>
2025-11-03 22:08:10 +0530

    day 1 pandas

--- Pandas/day1.py
+++ Pandas/day1.py
@@ -1,22 +1,5 @@
 import pandas as pd
 
-df = pd.DataFrame([[1,2,3], [4,5,6,], [7,8,9]], columns=["a", "b", "c"], index=["alpha", "beta", "gamma"])
-
-
-print(df) #shows entire df
-print("\n")
-print(df.head(2))   #df.head() by default prints first 5 rows of df, we can specify by df.head(3), shows first 2 rows
-print("\n")
-print(df.tail(2))  #shows last 2 rows
-
-
-print(df.columns)   #Index(['a', 'b', 'c'], dtype='object')
-print(df.index)    #Index(['alpha', 'beta', 'gamma'], dtype='object')
-
-print(df.columns.tolist())    # ['a', 'b', 'c']
-print(df.index.tolist())    # ['alpha', 'beta', 'gamma']
-
-df = pd.read_csv("pokemon.csv")
-print(df)
+#series is 1d labeled array, 
 
 
